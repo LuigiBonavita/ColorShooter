f912b36d4aca0836c6e7eb9cff449a6c55f0f3b9
MI_C_07\BONAVITAL <luigi.bonavita21@gmail.com>
2018-04-19 17:47:04 +0200

    push1

---
+++ Assets/Scripts/EventManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventManager : MonoBehaviour {
+
+
+    public delegate void Killable();
+    public static event Killable Killed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void TurnColor()
+    {
+        /*if (scrivere la condizione)
+        {
+            if (Killed != null)
+                Killed();
+        }*/
+    }
+}
---
+++ Assets/Scripts/ShootInput.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ShootInput : MonoBehaviour
+{
+    [Header("Input Settings")]
+
+    Vector3 hitpoint;
+    public KeyCode ShootKey = KeyCode.R;
+    public float ShootForce = 0.3f;
+    public Transform ShootPoint;
+    public bool IsShooting = false;
+    public float Timer = 0f;
+
+    PlayerBulletPoolManager bulletManagers;
+
+    // Use this for initialization
+    void Start()
+    {
+        bulletManagers = FindObjectOfType<PlayerBulletPoolManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        RaycastHit hit;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+           hitpoint = hit.point;
+        }
+
+        transform.DOLookAt(hitpoint, 0.2f);
+
+
+        if (Input.GetKey(ShootKey) && IsShooting == false)
+        {
+            transform.DOLookAt(new Vector3(0,1),01);// scrivere meglio la direzione
+            Shoot();
+            IsShooting = true;
+
+        }
+        if (IsShooting == true)
+        {
+
+            Timer = Timer + Time.deltaTime;
+        }
+        if (Timer >= 0.3f)
+        {
+            IsShooting = false;
+            Timer = 0f;
+        }
+        
+    }
+
+    public void Shoot()
+    {
+        PlayerBullet bulletToShoot = bulletManagers.GetBullet();
+        bulletToShoot.transform.position = ShootPoint.position;
+        bulletToShoot.Shoot(transform.forward, ShootForce);
+        bulletToShoot.OnDestroy += OnBulletDestroy;
+    }
+
+    public void OnBulletDestroy(PlayerBullet bullet)
+    {
+
+        bullet.OnDestroy -= OnBulletDestroy;
+
+    }
+}
+
